da94a874f53b47014925a98ac1b5fcfbdd0e1534
Mn <mininoz@gmail.com>
2015-04-09 00:29:09 +0700

    add role to user table and defualt new register user with user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum role: [:admin, :user]
+  before_create :set_default_role
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -8,4 +11,9 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :listings
+
+  private
+  def set_default_role
+    self.role ||= 'user'
+  end
 end
